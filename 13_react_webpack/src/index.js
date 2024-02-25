70184213dc3eabc41f69c16edb904b05386bf051
lamhuynhb779 <lamhuynhb779@gmail.com>
2024-02-26 00:18:39 +0700

    create new react app & config webpack

---
+++ 13_react_webpack/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react' // nap thu vien react
+import ReactDOM from 'react-dom' // nap thu vien react-dom
+
+// Tao component App
+function App() {
+    return (
+        <div>
+            <h1>Hello guys!</h1>
+            <h2>...</h2>
+            <h3>End</h3>
+        </div>
+    )
+}
+
+// Render component App vao #root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
